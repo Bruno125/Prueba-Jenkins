7ee82316621ea7990c7306d1e778ec731fb03182
Bruno Aybar <miguel_bruno@hotmail.com>
2015-06-02 20:37:44 -0500

    Primer commit

---
+++ Hello jenkins/Hello jenkins.cpp
@@ -0,0 +1,12 @@
+// Hello jenkins.cpp: archivo de proyecto principal.
+
+#include "stdafx.h"
+
+using namespace System;
+
+int main(array<System::String ^> ^args)
+{
+    Console::WriteLine(L"Hola a todos");
+	Console::WriteLine(L"Primer commit");
+    return 0;
+}
