fed9cf40685f8ab5b8c456fd2cbdc574a00fdfd8
Bruno Aybar <miguel_bruno@hotmail.com>
2015-06-03 19:26:35 -0500

    Tercera clase añadida

---
+++ Hello jenkins/TerceraClase.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "TerceraClase.h"
+
+
+TerceraClase::TerceraClase()
+{
+}
+
+
+TerceraClase::~TerceraClase()
+{
+}
---
+++ Hello jenkins/TerceraClase.h
@@ -0,0 +1,8 @@
+#pragma once
+class TerceraClase
+{
+public:
+	TerceraClase();
+	~TerceraClase();
+};
+
